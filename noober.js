43d6448a184948fcc5f149dd21a278f3b1feb4f1
Lili Myers <lilianna.myers@kellogg.northwestern.com>
2021-02-03 11:24:47 -0600

    wip

--- noober.js
+++ noober.js
@@ -7,6 +7,25 @@ async function pageLoaded() {
   
   // 🔥 start here: write code to loop through the rides
   
+  let OutputElement = document.querySelector('.rides')
+  for (let i=0; i < json.length; i++){
+
+    if(json[i].length > 1) {
+    levelOfSevice = 'Noober Pool'
+    OutputElement.insertAdjacentHTML('beforeend',`
+    <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+    <i class="fas fa-car-side"></i>
+    <span>${levelOfService}</span>
+    </h1>`)
+  }
+
+
+  }
+
+
+
+
+
 }
 
 window.addEventListener('DOMContentLoaded', pageLoaded)
